0d37df0d3d4568b6d07609b236e2b0f956e70c1c
deltacorporate <houms937@gmail.com>
2022-01-03 13:38:10 +0100

    -finalisation update des settings et des détails -ajout d'envoi d'email à l'update de password et à la suppression de compte -ajout de variable emails dans le fichier de config.

---
+++ www/app/Middlewares/CsrfMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+/*
+*@Author: Houmame LAZAR <houms937@gmail.com>
+*@Class: AuthMiddleware
+*@NameSpace: App\Middlewares
+*/
+
+namespace App\Middlewares;
+
+use Core\Csrf;
+use Core\Middleware;
+use Core\Request;
+use Core\Session;
+
+class CsrfMiddleware extends Middleware
+{
+
+    public function run(Request $request, Session $session): bool
+    {
+        $token = Request::postBody()["csrf"];
+        $tokens = Session::session("csrf");
+        foreach ($tokens as $key => $csrf) {
+            if ($csrf->getToken() == $token and time() < $csrf->getExpireAt()) {
+                unset($tokens[$key]);
+                Session::setSession("csrf", $tokens);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function error()
+    {
+        return render("errors.419");
+    }
+}
